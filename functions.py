9938a8ab9c45b6c0ca047d87d96c59dcd9bb34b8
Will Poulett <william.poulett1@nhs.net>
2024-04-09 15:35:54 +0100

    no csvs

--- functions.py
+++ functions.py
@@ -6,6 +6,7 @@ from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNo
 from keras.preprocessing.image import ImageDataGenerator
 from keras import utils
 import random
+import pandas as pd
 
 def split_data(train_dataset, test_dataset, val_dataset, RANDOM_SEED = 1, one_hot_encoded = True, num_classes = 2, image_size = 128):
     """_summary_
@@ -120,7 +121,7 @@ def generate_training_mixup_images(X_train, y_train, ITERS = 1):
             while new_label == False:
                 id=random.randint(0,len(y_train)-1)
                 # Check label is from other class
-                if y_train[id][0] != y1[0]:
+                if np.argmax(y_train[id]) != np.argmax(y1):
                     new_label = True
                     X2, y2 = X_train[id], y_train[id]
 
@@ -129,3 +130,123 @@ def generate_training_mixup_images(X_train, y_train, ITERS = 1):
 
     return X_train_mixup, y_train_mixup
         
+
+def generate_testing_mixup_images(test_set_A_df, X_test_A, MUBA_ITERS, classes = 2):
+    # Appending to arrays is more efficient than appending a row at a time to a df.
+    alphas = {}
+    class_indexes = {}
+    mixup_image = []
+    labels = []
+
+    for class_num in range(classes):
+        alphas[f"alpha_class_{str(class_num)}"] = []
+        class_indexes[f"class_{str(class_num)}_index"] = []
+
+    for class_num in range(classes-1): # Iterate through the class numbers
+        for index_0, row_0 in test_set_A_df[test_set_A_df["class"]==class_num].iterrows(): # For each class X image
+            for index_1, row_1 in test_set_A_df[test_set_A_df["class"]>class_num].iterrows(): # For each class > X image
+                class_row_1 = row_1["class"]
+                for i in range(MUBA_ITERS):
+                    alpha = (1/MUBA_ITERS) * np.random.rand() + ( (i) / MUBA_ITERS) # Create an alpha value inside a bin of width 1/MUBA_ITERS
+                    new_img = alpha * X_test_A[int(row_0["image_index"])] + (1 - alpha) * X_test_A[int(row_1["image_index"])]
+                    
+                    label = class_num if alpha > 0.5 else class_row_1 # If alpha is greater than 0.5, there is a higher proportion of a class 0 image.
+                    
+                    for c in range(classes):
+                        if c == class_num:
+                            alphas[f"alpha_class_{str(c)}"].append(alpha)
+                            class_indexes[f"class_{str(c)}_index"].append(int(index_0))
+                        elif c == class_row_1:
+                            alphas[f"alpha_class_{str(c)}"].append(float(1-alpha))
+                            class_indexes[f"class_{str(c)}_index"].append(int(index_1))
+                        else:
+                            alphas[f"alpha_class_{str(c)}"].append(0)
+                            class_indexes[f"class_{str(c)}_index"].append(None)
+
+                    mixup_image.append(new_img)
+                    labels.append(label)
+
+    images_types_labels = {"image": mixup_image,
+                         "type":["mix" for i in range(len(labels))],
+                         "label":labels}
+    merged_dict = alphas | class_indexes | images_types_labels
+
+    return pd.DataFrame(merged_dict)
+
+def find_boundary_points(muba_df,
+                         X_test_A,
+                         MUBA_ITERS: int = 60,
+                         classes=2):
+    """Generates new images with alpha values between those at which a model changes it's prediction
+
+    Args:
+        muba_df (DataFrame): DataFrame containing all mixed up images
+        MUBA_ITERS (int, optional): _description_. Defaults to 60.
+
+    Returns:
+        boundary_points_df (DataFrame): Contains all boundary points
+    """
+
+    # Appending to arrays is more efficient than appending a row at a time to a df.
+    alphas = {}
+    class_indexes = {}
+    mixup_image = []
+    labels = []
+
+    for class_num in range(classes):
+        alphas[f"alpha_class_{str(class_num)}"] = []
+        class_indexes[f"class_{str(class_num)}_index"] = []
+
+    for i in range(int((len(muba_df))/MUBA_ITERS)):
+
+        # Create a mask to split df in to blocks of MUBA_ITERS
+        mask = (muba_df.index >= MUBA_ITERS*i) & (muba_df.index < MUBA_ITERS*i + MUBA_ITERS)
+        window_df = muba_df.loc[mask]
+
+        # Find the index in which the prediction changes
+        changing_pred_index = (window_df["argmax_pred"].diff()[window_df["argmax_pred"].diff() != 0].index.values)
+        for index, row in window_df.iterrows():
+            
+            if index in changing_pred_index[1:]:
+                
+                row0 = window_df.loc[[index]]
+                row1 = window_df.loc[[index-1]]
+                
+                row_0_class_0 = row0["label"][index]
+                
+                # Get id for both images
+                image_0_id = int(row0[f"class_{row_0_class_0}_index"]) # Find id of image with largest alpha
+                index_columns = list(class_indexes.keys())
+                for id_col in index_columns:
+                    id = row0[id_col][index]
+                    if not np.isnan(id) and id != image_0_id:
+                        image_1_id = int(id)
+                        row_0_class_1 = [int(s) for s in id_col.split("_") if s.isdigit()][0]
+                        
+                # Work out alpha class 0 as the difference between alpha class 0 in both images
+                alpha_class_0 = ( float(row0[f"alpha_class_{row_0_class_0}"]) + float(row1[f"alpha_class_{row_0_class_0}"])) / 2
+                alpha_class_1 = 1 - alpha_class_0
+                image = alpha_class_0 * X_test_A[image_0_id] + alpha_class_1 * X_test_A[image_1_id]
+                
+                label = row_0_class_0 if alpha_class_0 > 0.5 else row_0_class_1
+                
+                for c in range(classes):
+                    if c == row_0_class_0:
+                        alphas[f"alpha_class_{str(c)}"].append(alpha_class_0)
+                        class_indexes[f"class_{str(c)}_index"].append(image_0_id)
+                    elif c == row_0_class_1:
+                        alphas[f"alpha_class_{str(c)}"].append(float(alpha_class_1))
+                        class_indexes[f"class_{str(c)}_index"].append(image_1_id)
+                    else:
+                        alphas[f"alpha_class_{str(c)}"].append(0)
+                        class_indexes[f"class_{str(c)}_index"].append(None)
+
+                mixup_image.append(image)
+                labels.append(label)
+
+    images_types_labels = {"image": mixup_image,
+                         "type":["boundary" for i in range(len(labels))],
+                         "label":labels}
+    merged_dict = alphas | class_indexes | images_types_labels
+
+    return pd.DataFrame(merged_dict)
--- plotting_functions.py
+++ plotting_functions.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import seaborn as sn
+from sklearn.calibration import calibration_curve
+import pandas as pd
+
 
 def plot_history(history, size = (20,10), epochs = 20):
     epochs = [i for i in range(epochs)]
@@ -22,4 +28,112 @@ def plot_history(history, size = (20,10), epochs = 20):
     ax[1].legend()
     ax[1].set_xlabel("Epochs")
     ax[1].set_ylabel("Training & Validation Loss")
+    plt.show()
+
+def muba_curve(type,
+               values,
+               bins = 20,
+               average = True,
+               title = None,
+               fig_size = (10,8),
+               xlabel="Alpha",
+               ylabel="N"):
+    
+    assert type == "error" or type == "boundary"
+
+    fig, ax = plt.subplots(figsize=fig_size)
+    
+    ax.hist(values,bins=[i/bins for i in range(bins+1)])
+    if average:
+        ax.axvline(np.mean(values), linestyle = "--", label = f"Mean: {round(np.mean(values),2)}")
+
+    ax.legend()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+def plot_3_confusion_matricies(mixup_images_df,true_images_A_df,true_images_B_df, labels = ["Normal","Pneumonia"]):
+
+    fig, axs = plt.subplots(1,3, figsize = (15,5))
+    cm_muba = confusion_matrix([x for x in mixup_images_df["label"]],[y for y in mixup_images_df["argmax_pred"]])
+    cm_test_A = confusion_matrix([x for x in true_images_A_df["label"]],[y for y in true_images_A_df["argmax_pred"]])
+    cm_test_B = confusion_matrix([x for x in true_images_B_df["label"]],[y for y in true_images_B_df["argmax_pred"]])
+
+    cm_muba_display = ConfusionMatrixDisplay(confusion_matrix = cm_muba, display_labels=labels)
+    cm_test_A_display = ConfusionMatrixDisplay(confusion_matrix = cm_test_A, display_labels=labels)
+    cm_test_B_display = ConfusionMatrixDisplay(confusion_matrix = cm_test_B, display_labels=labels)
+
+    cm_muba_display.plot(ax = axs[0])
+    cm_test_A_display.plot(ax = axs[1])
+    cm_test_B_display.plot(ax = axs[2])
+
+    fig.tight_layout()
+
+    axs[0].set_title("Mixup Data")
+    axs[1].set_title("Test A Data")
+    axs[2].set_title("Test B Data")
+    plt.show()
+
+def plot_calibration(true_images_A_df,true_images_B_df,mixup_images_df,boundary_images_df, positive_class = "Normal"):
+
+    fig, axs = plt.subplots(1,2,figsize = (10,5))
+
+    prob_demented_A, prob_pred_A = calibration_curve(true_images_A_df["label"],true_images_A_df["argmax_pred"],n_bins=10)
+    prob_demented_B, prob_pred_B = calibration_curve(true_images_B_df["label"],true_images_B_df["argmax_pred"],n_bins=10)
+    prob_demented_mix, prob_pred_mix = calibration_curve(mixup_images_df["label"],mixup_images_df["argmax_pred"],n_bins=10)
+
+    # The proportion of samples whose class is the positive class, in each bin (fraction of positives)
+    # The mean predicted probability in each bin.
+
+    alpha_confidence_df = pd.DataFrame({"alpha":mixup_images_df["alpha_class_0"],"pred":mixup_images_df["predictions_0"]}).sort_values("pred")
+    bins = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+    alpha_confidence_df["bins"] = pd.cut(alpha_confidence_df["pred"],bins)
+    new_df = alpha_confidence_df.groupby("bins").mean()
+    mean_alphas = new_df["alpha"]
+
+    axs[1].scatter(mixup_images_df["predictions_0"],mixup_images_df["alpha_class_0"], alpha = 0.01, label = "Mixup Data")
+    axs[1].scatter(boundary_images_df["predictions_0"],boundary_images_df["alpha_class_0"], alpha = 0.1, label = "Boundary Points")
+
+    for i in range(10):
+        plt.axvline(i/10,linestyle="--",color = "black",alpha=0.1)
+
+    middle_bins = [0.05,0.15,0.25,0.35,0.45,0.55,0.65,0.75,0.85,0.95]
+    axs[1].plot(middle_bins,mean_alphas,marker = ".",label = "Mean Alpha",color = "black")
+    axs[1].plot([0,1],[0,1],linestyle="--",color = "black",alpha=0.5)
+    axs[1].set_ylabel(f"Alpha Value (Class: {positive_class})")
+    axs[1].set_xlabel(f"Prediction Confidence (Class: {positive_class})")
+
+    plt.legend(loc="lower right")
+
+    axs[0].set_xlabel("Mean Predicted Probability (Positive Class: {positive_class})")
+    axs[0].set_ylabel("Fraction of Positives (Positive Class: {positive_class})")
+    axs[0].set_title("Calibration Curve")
+
+    axs[0].plot(prob_pred_A,prob_demented_A,marker = ".", label = "Test Set A")
+    axs[0].plot(prob_pred_B,prob_demented_B,marker = ".", label = "Test Set B")
+    axs[0].plot(prob_pred_mix,prob_demented_mix,marker = ".", label = "Mixup")
+    axs[0].plot([0, 1], [0, 1], linestyle='--',color="black", label = "Perfect Calibration")
+    axs[0].legend()
+
+    plt.show()
+
+def muba_curves(mixup_errors,boundary_images_df, average = True, bins = 20, xlabel = "Proportion Class 0"):
+    fig, axs = plt.subplots(1,2, figsize = (10,5))
+
+    axs[0].hist(mixup_errors["alpha_class_0"],bins=[i/bins for i in range(bins+1)],)
+    axs[0].set_ylabel("N")
+    axs[0].set_xlabel(xlabel)
+    axs[0].set_title("Error Distribution Curve")
+
+    axs[1].hist(boundary_images_df["alpha_class_0"],bins=[i/bins for i in range(bins+1)],color="green")
+    axs[1].set_ylabel("N")
+    axs[1].set_xlabel(xlabel)
+    axs[1].set_title("Boundary Distribution Curve")
+
+    if average:
+        axs[0].axvline(np.mean(mixup_errors["alpha_class_0"]), linestyle = "--",color="black", label = f"Mean: {round(np.mean(mixup_errors['alpha_class_0']),2)}")
+        axs[0].legend()
+        axs[1].axvline(np.mean(boundary_images_df["alpha_class_0"]), linestyle = "--",color="black", label = f"Mean: {round(np.mean(boundary_images_df['alpha_class_0']),2)}")
+        axs[1].legend()
+
     plt.show()
